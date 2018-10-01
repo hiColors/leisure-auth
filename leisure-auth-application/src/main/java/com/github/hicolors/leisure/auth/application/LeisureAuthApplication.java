6b603e1255f7a302e21323efab17f79098601bcd
李伟超 <liweichao0102@gmail.com>
2018-10-02 00:05:14 +0800

    Initial commit

---
+++ leisure-auth-application/src/main/java/com/github/hicolors/leisure/auth/application/LeisureAuthApplication.java
@@ -0,0 +1,19 @@
+package com.github.hicolors.leisure.auth.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 鉴权服务
+ *
+ * @author weichao.li (liweichao0102@gmail.com)
+ * @date 2018/10/2
+ */
+@SpringBootApplication
+public class LeisureAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeisureAuthApplication.class, args);
+    }
+
+}
