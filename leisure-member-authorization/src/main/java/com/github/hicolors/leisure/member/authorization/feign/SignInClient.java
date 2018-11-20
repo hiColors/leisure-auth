04200d424f496d7875b86a7e9d8e80715a08f0ad
李伟超 <liweichao0102@gmail.com>
2018-11-20 23:11:17 +0800

    - 增加切换平台接口

--- leisure-member-authorization/src/main/java/com/github/hicolors/leisure/member/authorization/feign/SignInClient.java
+++ leisure-member-authorization/src/main/java/com/github/hicolors/leisure/member/authorization/feign/SignInClient.java
@@ -2,6 +2,7 @@ package com.github.hicolors.leisure.member.authorization.feign;
 
 import com.github.hicolors.leisure.member.authorization.token.impl.MemberAuthorization;
 import com.github.hicolors.leisure.member.model.persistence.Member;
+import com.github.hicolors.leisure.member.model.persistence.Platform;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.cloud.openfeign.FeignClient;
@@ -16,30 +17,33 @@ import org.springframework.web.bind.annotation.RequestParam;
  * @author weichao.li (liweichao0102@gmail.com)
  * @date 2018/11/16
  */
-@Api(tags = "member", description = "登录的相关操作")
+@Api(tags = "BackendGatewaySignIn", description = "后台网关登录的相关操作")
 @FeignClient(name = "leisure-member", path = "/leisure-member")
-@RequestMapping("member")
 public interface SignInClient {
 
 
     @ApiOperation("人员 - 获取人员信息通过 id")
-    @GetMapping("/{id}")
+    @GetMapping("/member/{id}")
     Member queryOneById(@PathVariable("id") Long id);
 
     @ApiOperation("人员 - 获取人员信息通过 mobile")
-    @GetMapping("/mobile")
+    @GetMapping("/member/mobile")
     Member queryOneByMobile(@RequestParam("mobile") String mobile);
 
     @ApiOperation("人员 - 获取人员信息通过 email")
-    @GetMapping("/email")
+    @GetMapping("/member/email")
     Member queryOneByEmail(@RequestParam("email") String email);
 
     @ApiOperation("人员 - 获取人员信息通过 uniquekey（包含用户名，手机号，邮箱） 和 password")
-    @GetMapping("/unique-key/password")
-    Member queryOneByUniqueKeyAndPassword(@RequestParam("uniquekey") String uniquekey, @RequestParam("password") String password);
+    @GetMapping("/member/unique-key/password")
+    Member queryOneByUniqueKeyAndPassword(@RequestParam("uniquekey") String uniquekey,
+                                          @RequestParam("password") String password);
 
     @ApiOperation("人员 - 获取人员所有的授权信息")
-    @GetMapping("/{id}/authorization")
+    @GetMapping("/member/{id}/authorization")
     MemberAuthorization queryMemberAuthorization(@PathVariable("id") Long id);
 
+    @ApiOperation("平台 - 查询")
+    @GetMapping("/platform/{id}")
+    Platform queryPlatform(@PathVariable("id") Long id);
 }
