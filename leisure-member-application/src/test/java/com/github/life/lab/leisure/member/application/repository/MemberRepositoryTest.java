ac4ca904204e7b170b19cb2526f7004b17549bd8
liulangdeyufcb <2835265478@qq.com>
2018-11-27 23:45:35 +0800

    MemberRepository部分测试

---
+++ leisure-member-application/src/test/java/com/github/life/lab/leisure/member/application/repository/MemberRepositoryTest.java
@@ -0,0 +1,47 @@
+package com.github.life.lab.leisure.member.application.repository;
+
+import com.github.life.lab.leisure.member.model.persistence.Member;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * @ClassName MemberRepositoryTest
+ * @Description TODO
+ * @Author fhero
+ * @Date 2018-11-27 23:17
+ * @Version 1.0
+ **/
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@Slf4j
+@ActiveProfiles("dev")
+public class MemberRepositoryTest {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+
+    @Test
+    public void findByUsername(){
+        log.info("成员信息:{}",memberRepository.findByUsername("liweichao"));
+    }
+
+    @Test
+    public void findByUsernameAndPassword01(){
+        Assert.assertEquals(null,memberRepository.findByUsernameAndPassword("fff","ff"));
+    }
+
+    @Test
+    public void findByUsernameAndPassword02(){
+        log.info("{}",memberRepository.findByUsernameAndPassword("liweichao","000000"));
+        Assert.assertNotNull(memberRepository.findByUsernameAndPassword("liweichao","000000"));
+    }
+
+
+}
--- leisure-member-application/src/test/java/com/github/life/lab/leisure/member/application/repository/PlatformRepositoryTest.java
+++ leisure-member-application/src/test/java/com/github/life/lab/leisure/member/application/repository/PlatformRepositoryTest.java
@@ -3,16 +3,20 @@ package com.github.life.lab.leisure.member.application.repository;
 import com.github.life.lab.leisure.common.utils.JsonUtils;
 import com.github.life.lab.leisure.member.model.consts.EnumPlatformStatus;
 import com.github.life.lab.leisure.member.model.persistence.Platform;
+import com.oracle.tools.packager.Log;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 @Slf4j
+@ActiveProfiles("dev")
 public class PlatformRepositoryTest {
 
     @Autowired
@@ -30,4 +34,27 @@ public class PlatformRepositoryTest {
         log.info("{}", platformRepository.count());
     }
 
-}
+    @Test
+    public void findByName01(){
+        String name = "";
+        log.info("信息是:{}",platformRepository.findByName(name));
+        Assert.assertNotNull(platformRepository.findByName(name));
+    }
+
+    @Test
+    public void findByName02(){
+        String name = "Life Lab";
+        log.info("信息是:{}",platformRepository.findByName(name));
+        Assert.assertNotNull(platformRepository.findByName(name));
+    }
+
+    @Test
+    public void findByName03() {
+        String name = " Lab";
+        log.info("信息是:{}", platformRepository.findByName(name));
+        Assert.assertNotNull(platformRepository.findByName(name));
+    }
+
+
+
+    }
