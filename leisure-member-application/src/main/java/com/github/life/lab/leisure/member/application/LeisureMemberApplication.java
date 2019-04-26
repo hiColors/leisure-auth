813417c80b6094f6f1b4ddad2b2735c999dcce93
liulangdeyufcb <2835265478@qq.com>
2019-04-26 14:39:56 +0800

    提交

--- leisure-member-application/src/main/java/com/github/life/lab/leisure/member/application/LeisureMemberApplication.java
+++ leisure-member-application/src/main/java/com/github/life/lab/leisure/member/application/LeisureMemberApplication.java
@@ -3,6 +3,7 @@ package com.github.life.lab.leisure.member.application;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 /**
  * 人员管理服务
@@ -15,7 +16,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LeisureMemberApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LeisureMemberApplication.class, args);
+        ApplicationContext applicationContext = null;
+        boolean success = true;
+        try {
+            applicationContext = SpringApplication.run(LeisureMemberApplication.class, args);
+        } catch (Exception e) {
+            success = false;
+            log.info("*****************************启动失败*******************************", e);
+        }
+        if (success) {
+            log.info("*****************************启动成功*******************************");
+        }
     }
-
 }
